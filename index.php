db451b8fe6fc7c4f35524d8da00a8f2318b289fd
Ray Aguilar <flexgrip@gmail.com>
2010-03-22 17:03:39 -0500

    Layout Finished

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php //reserved for session_start ?>
 <?php
-print "test";
+// Programming - Ray Aguilar
+// Project Goal - Facilitate buyers in configuring their projects/books
+// Based on - PHP, MySQL, HTML, CSS
+// Date - March 15th, 2010
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<link rel = "stylesheet" type="text/css" href="style.css" >
+</head>
+<body>
+
+	<div id="container">
+		<div id="header">
+			<div id="otb-logo"></div>
+			<div id="cl-logo"></div>
+		</div>
+		
+		<div id="content-wrapper">
+			<div id="top-bar"></div>
+			<div id="top-bar-spacer"></div>
+			
+			<!-- BEGIN CONTENT -->
+			<div class="content">
+				<h1 class="page-title">Greetings Earthling,</h1>
+				<div id="heading-spacer"></div>
+				<div class="the-box">
+					<div class="the-title"><h3 class="the-heading">To-Do List</h3></div><img class="the-box-bg" src="images/boxbg.png" width="100%" height="100%" />
+				</div>
+				
+			<div style="clear: both;"></div>	
+			</div>
+			<!-- END CONTENT -->
+		</div>
+	
+	
+	</div><!-- END #CONTAINER -->
+
+</body>
+</html>
